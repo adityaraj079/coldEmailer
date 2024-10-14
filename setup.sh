af7f55ff07f9523ae8596f6d099b73534d560ff5
Aditya Raj <rajaditya079@gmail.com>
2024-10-14 14:33:23 +0530

    Setup added

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y \
+  libglib2.0-0 \
+  libnss3 \
+  libgconf-2-4 \
+  libxss1 \
+  libx11-xcb1 \
+  libxcomposite1 \
+  libxcursor1 \
+  libxi6 \
+  libxtst6 \
+  libxrandr2 \
+  libasound2 \
+  fonts-liberation \
+  libappindicator1 \
+  libappindicator3-1 \
+  libatk-bridge2.0-0 \
+  libatk1.0-0 \
+  libcups2 \
+  libxss1 \
+  libxshmfence1
